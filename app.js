7946d23084cc06be2df951208364c59d56d2991f
Fabian <hahn.fabian@proton.me>
2023-02-17 00:59:04 +0100

    hacking effect

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("").map((letters, index) => {
+        if(incex < iterations) {
+            return event.target.dataset.value[index];
+        }
+
+        return letter[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+    
+    
+    if (iterations >= event.target.dataset.value.length) {
+        clearInterval(interval);
+    }
+
+    iterations += 1 / 3;
+    }, 30);
+}
