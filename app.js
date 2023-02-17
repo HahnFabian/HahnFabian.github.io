df939581abad3c523e5fc161950c54e7eb95f010
Fabian <hahn.fabian@proton.me>
2023-02-17 01:06:52 +0100

    changes

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
 
 document.querySelector("h1").onmouseover = event => {
     let iterations = 0;
 
+    clearInterval(interval);
+
     const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText.split("").map((letters, index) => {
-        if(incex < iterations) {
-            return event.target.dataset.value[index];
+        event.target.innerText = event.target.innerText.split("").map((letters, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+
+            return letter[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+        
+        
+        if (iterations >= event.target.dataset.value.length){
+            clearInterval(interval);
         }
 
-        return letter[Math.floor(Math.random() * 26)]
-    })
-    .join("");
-    
-    
-    if (iterations >= event.target.dataset.value.length) {
-        clearInterval(interval);
-    }
-
-    iterations += 1 / 3;
+        iterations += 1 / 3;
     }, 30);
 }
