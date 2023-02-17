e069725de508bf68d587abc5d71e1194b6829abc
Fabian <hahn.fabian@proton.me>
2023-02-17 01:23:04 +0100

    aaa

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 let interval = null;
 
-document.querySelector("h1").onmouseover = event => {
-    let iterations = 0;
-
-    clearInterval(interval);
-
-    const interval = setInterval(() => {
-        event.target.innerText = event.target.innerText.split("").map((letters, index) => {
-            if(index < iterations) {
-                return event.target.dataset.value[index];
-            }
-
-            return letter[Math.floor(Math.random() * 26)]
-        })
-        .join("");
-        
-        
-        if (iterations >= event.target.dataset.value.length){
-            clearInterval(interval);
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
         }
-
-        iterations += 1 / 3;
-    }, 30);
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
 }
